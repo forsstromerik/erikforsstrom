7e598dc3a6d0b3d6aeae046de46a3b1bbc0db214
Erik Forsström <erikforsstrom.kode@gmail.com>
2018-05-23 21:28:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{fontSize: "100px", width: "100%", padding: "40px", textAlign: "center"}}>
+        <div> Erik Forsström </div>
+      </div>
+    );
+  }
+}
+
+export default App;
